a30e70ed29da8b2d5c31e0949f17ff870d209520
Bob <tpitorin@myges.fr>
2023-06-09 14:04:13 +0200

    Add 4s delay for error message

--- app/src/main/java/com/sorcierstechnologiques/cookmaster/MainActivity.java
+++ app/src/main/java/com/sorcierstechnologiques/cookmaster/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
                     MainActivity.this.startActivity(logged);
                 } else {
                     error.setText("Email ou mot de passe incorrect");
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            error.setText("");
+                        }
+                    }, 4000);
+                
                 }
 
             }
