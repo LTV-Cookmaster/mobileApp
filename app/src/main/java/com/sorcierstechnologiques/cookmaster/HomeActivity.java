c274f209ff2c6205fa0dfef1cd261bf217aa0a44
Vincent MECHOUCHE <vme@myges.fr>
2023-06-19 23:33:46 +0200

    Adding burger menu (in progress)

--- app/src/main/java/com/sorcierstechnologiques/cookmaster/HomeActivity.java
+++ app/src/main/java/com/sorcierstechnologiques/cookmaster/HomeActivity.java
@@ -2,46 +2,91 @@ package com.sorcierstechnologiques.cookmaster;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button btnquit;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        this.btnquit = findViewById(R.id.btn_quit);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
 
-        this.btnquit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                /*Toast.makeText(MainActivity.this,"Catch this ratio !",Toast.LENGTH_SHORT).show();*/
-                new AlertDialog.Builder(HomeActivity.this)
-                        .setTitle("Quitter ?")
-                        .setMessage("Voulez vous quittez ?")
-                        .setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+        setSupportActionBar(toolbar);
 
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                finish();
-                            }
-                        })
-                        .setNegativeButton("Non", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
 
-                            }
-                        })
-                        .setCancelable(false)
-                        .show();
-            }
-        });
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem(R.id.toolbar);
+        super.onPrepareOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.btn_fidelity){
+            Intent fidelityIntent = new Intent(HomeActivity.this, FidelityActivity.class);
+            startActivity(fidelityIntent);
+
+            return true;
+        } else if (id == R.id.btn_profile){
+            Intent profileIntent = new Intent(HomeActivity.this, ProfileActivity.class);
+            startActivity(profileIntent);
+
+            return true;
+        } else if (id == R.id.btn_home){
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
