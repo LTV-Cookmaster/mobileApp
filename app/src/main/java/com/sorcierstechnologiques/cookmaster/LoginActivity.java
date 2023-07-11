75e9d1b361e605f2b342a6ead7bbb94619263d2e
Bob <tpitorin@myges.fr>
2023-07-12 00:04:31 +0200

    add delay for error message

--- app/src/main/java/com/sorcierstechnologiques/cookmaster/LoginActivity.java
+++ app/src/main/java/com/sorcierstechnologiques/cookmaster/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -85,7 +86,13 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 }, error -> {
                     errorText.setText("Email ou mot de passe incorrect");
-                }) {
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            errorText.setText("");
+                        }
+                    }, 3000);                }) {
                     @Override
                     public String getBodyContentType() {
                         return "application/json; charset=utf-8";
